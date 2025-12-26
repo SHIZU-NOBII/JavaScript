0ffb36704243ca3f8acde8286b576a2474d215f8
SHIZU-NOBII <137601319+SHIZU-NOBII@users.noreply.github.com>
2025-12-26 12:19:43 +0500

    Module Pattern

---
+++ 04-JS Design Pattern/main.js
@@ -0,0 +1,35 @@
+console.log("Working");
+alert("MAIN JS EXECUTED");
+
+let Bank = (function () {
+  let bankBalance = 1000; // private variable
+
+  function checkBalance() {
+    console.log("Your Balance is " + bankBalance);
+  }
+
+  function withdraw(amount) {
+    if (amount > bankBalance) return "Insufficient Balance";
+
+    bankBalance -= amount;
+    console.log("Withdraw Successful. Remaining Balance: " + bankBalance);
+  }
+
+  function deposit(amount) {
+    if (amount < 100) return "Amount must be greater than or equal to 100";
+
+    bankBalance += amount;
+    console.log("Deposit Successful. Balance: " + bankBalance);
+  }
+
+  //public API
+  return {
+    checkBalance,
+    withdraw,
+    deposit,
+  };
+})();
+
+Bank.checkBalance();
+Bank.deposit(500);
+Bank.withdraw(200);
